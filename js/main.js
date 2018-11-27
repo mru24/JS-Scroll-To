58a6eb37cd72e9f9bc4a37eda98546d4b0a19ebe
Val Wroblewski <mru24@yahoo.com>
2018-11-27 00:04:09 +0000

    Edited

--- js/main.js
+++ js/main.js
@@ -1,30 +1,15 @@
 let element = document.querySelectorAll('.container');
 
 function scrollToNext(el) {
-  console.log(el);
+  // console.log(el);
   element[el].nextElementSibling.scrollIntoView({
     behavior: 'smooth'
   });
 }
 
 function scrollToPrev(el) {
-  console.log(el);
+  // console.log(el);
   element[el].previousElementSibling.scrollIntoView({
     behavior: 'smooth'
   });
 }
-
-
-// for (var i = 0; i < element.length; i++) {
-//   element[i].addEventListener('click', function () {
-//     console.log(element[i]);
-//     element[i].nextElementSibling.scrollIntoView({
-//         behavior: 'smooth'
-//       });
-//   })
-// }
-// element[0].addEventListener('click', function () {
-//   element[0].nextElementSibling.scrollIntoView({
-//       behavior: 'smooth'
-//     });
-// })
